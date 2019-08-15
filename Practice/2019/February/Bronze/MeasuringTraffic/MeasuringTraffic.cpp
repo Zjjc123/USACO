69082256d162175098c710d6370837306278135e
zjjc123 <zjjc123@hotmail.com>
2019-08-15 14:55:03 -0700

    Measuring Traffic 3/10

--- Practice/2019/February/Bronze/MeasuringTraffic/MeasuringTraffic.cpp
+++ Practice/2019/February/Bronze/MeasuringTraffic/MeasuringTraffic.cpp
@@ -32,7 +32,7 @@ struct Sensor
 
 int main()
 {
-    std::ifstream fin("TestData/1.in");
+    std::ifstream fin("traffic.in");
     int n;
     fin >> n;
 
@@ -44,6 +44,8 @@ int main()
 
     int eMin = 0;
     int eMax = 0;
+    int eMinChange = 0;
+    int eMaxChange = 0;
     bool eNone = false;
     for (int i = 0; i < n; i++)
     {   
@@ -62,6 +64,8 @@ int main()
                 int bTempMin;
                 int bTempMax;
                 fin >> bTempMin >> bTempMax;
+                bMin = std::max(bMin, bTempMin);
+                bMax = std::min(bMax, bTempMax);
             }
         }
         else if (type == "on")
@@ -74,8 +78,9 @@ int main()
             int bTempMax;
             fin >> bTempMin >> bTempMax;
 
-            bMinChange -= bTempMin;
-            bMaxChange -= bTempMax;
+            // subtracting so flipped
+            bMinChange -= bTempMax;
+            bMaxChange -= bTempMin;
 
         }
         else if (type == "off")
@@ -99,4 +104,74 @@ int main()
     bMin = std::max(0, bMin);
     bMax = std::max(0, bMax);
 
+    std::cout << bMin << std::endl;
+    std::cout << bMax << std::endl;
+    
+    fin.seekg(0, std::ios::beg);
+    fin >> n;
+    for (int i = 0; i < n; i++)
+    {   
+        String type;
+        fin >> type;
+        if (type == "none")
+        {
+            if (eNone == false)
+            {
+                eNone = true;
+                fin >> eMin >> eMax;
+                eMinChange = 0;
+                eMaxChange = 0;
+            }
+            else
+            {
+                int eTempMin;
+                int eTempMax;
+                fin >> eTempMin >> eTempMax;
+                eMin = std::max(eMin, eTempMin);
+                eMax = std::min(eMax, eTempMax);
+            }
+        }
+        else if (type == "on")
+        {
+            if (eNone == true)
+            {
+                eNone = false;
+            }
+            int eTempMin;
+            int eTempMax;
+            fin >> eTempMin >> eTempMax;
+            
+            eMinChange += eTempMin;
+            eMaxChange += eTempMax;
+
+        }
+        else if (type == "off")
+        {
+            if (eNone == true)
+            {
+                eNone = false;
+            }
+            int eTempMin;
+            int eTempMax;
+
+            fin >> eTempMin >> eTempMax;
+
+            // subtracting so flipped
+            eMinChange -= eTempMax;
+            eMaxChange -= eTempMin;
+        }
+    }
+    eMin += eMinChange;
+    eMax += eMaxChange;
+
+    eMin = std::max(0, eMin);
+    eMax = std::max(0, eMax);
+
+    std::cout << eMin << std::endl;
+    std::cout << eMax << std::endl;
+
+    std::ofstream fout("traffic.out");
+
+    fout << bMin << " " << bMax << std::endl;
+    fout << eMin << " " << eMax << std::endl;
 }
