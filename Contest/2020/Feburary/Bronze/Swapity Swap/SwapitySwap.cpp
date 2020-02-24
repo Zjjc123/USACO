3362ed87d5d7e28fba3be5573d80aae57f005d63
Jason Zhang <zjjc123@hotmail.com>
2020-02-23 21:04:23 -0800

    Swapity Swap

--- Contest/2020/Feburary/Bronze/Swapity Swap/SwapitySwap.cpp
+++ Contest/2020/Feburary/Bronze/Swapity Swap/SwapitySwap.cpp
@@ -12,12 +12,20 @@ LANG: C++
 #include <algorithm>   
 #include <vector>       
 
+template<typename T>
+bool isEqual(std::vector<T> const &v1, std::vector<T> const &v2)
+{
+    return (v1.size() == v2.size() &&
+            std::equal(v1.begin(), v1.end(), v2.begin()));
+}
+
 int main()
 {
     std::ifstream fin("swap.in");
     std::ofstream fout("swap.out");
 
     std::vector<int> cows;
+    std::vector<int> comp;
 
     int N;
     fin >> N;
@@ -31,13 +39,33 @@ int main()
     fin >> b1 >> b2;
 
     for (int i = 1; i <= N; i++) 
+    {
         cows.push_back(i);
+        comp.push_back(i);
+    }
 
+    bool breakearly = false;
     for (int i = 0; i < K; i++)
     {
         std::reverse(cows.begin() + a1 - 1, cows.begin() + a2);
         std::reverse(cows.begin() + b1 - 1, cows.begin() + b2);
+
+        if (isEqual(cows, comp))
+        {
+            K = K % (i + 1);
+            breakearly = true;
+            break;
+        }
     }
+    if (breakearly)
+    {
+        for (int i = 0; i < K; i++)
+        {
+            std::reverse(cows.begin() + a1 - 1, cows.begin() + a2);
+            std::reverse(cows.begin() + b1 - 1, cows.begin() + b2);
+        }
+    }
+
     for (int i = 0; i < N; i++) 
         fout << cows.at(i) << std::endl;
 }
